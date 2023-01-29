96b29211455c0cd6f7370c9980f2f87580a015dd
ragul0427 <121870806+ragul0427@users.noreply.github.com>
2023-01-29 14:46:22 +0530

    Update projectjs.js

--- projectjs.js
+++ projectjs.js
@@ -129,7 +129,10 @@ function getTotalRecords()
         
         totalRecords.innerText="Total records:"+res.data.length
 }).catch(error=>{
+  if(error)
    alert("Please connect your Data")
+   else
+   return res
 })
 
 }
